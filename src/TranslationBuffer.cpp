6ce8f692ca9e9f24b1030ea6110532005b244fbf
iamcamiel <iamcamiel>
2007-03-08 19:54:24 +0000

    Undid last change, need to fix...

--- src/TranslationBuffer.cpp
+++ src/TranslationBuffer.cpp
@@ -226,18 +226,13 @@ int CTranslationBuffer::convert_address(u64 virt, u64 *phys, u8 access, bool che
     if (cCPU->get_r(22+32,false)&X64(8000000000000000))
       return E_NOT_FOUND;
     va_form = cCPU->va_form(virt,bIBOX);
-    if (bIBOX)
-    {
-      if (cCPU->get_tb(false)->convert_address(va_form, &phys_pte, 0, false, 0, cCPU->get_asn(false), cCPU->get_spe(false), &b))
-	return E_NOT_FOUND;
-    }
-    else
-    {
-      i = FindEntry(va_form);
-      if (i<0)
-        return E_NOT_FOUND;
-      phys_pte = (entry[i].phys & v_mask) | (va_form & p_mask);
-    }
+
+    // manually translate va_form to a physical address to avoid recursion (which would occur when using convert_address)
+    i = FindEntry(va_form);
+    if (i<0)
+      return E_NOT_FOUND;
+    phys_pte = (entry[i].phys & v_mask) | (va_form & p_mask);
+    
     pte = systm->ReadMem(phys_pte,64);
     if (!(pte&1))
       return E_NOT_FOUND;
