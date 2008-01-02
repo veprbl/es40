ae56827df93c1c1a3713927798ec4bdaaaebe556
iamcamiel <iamcamiel>
2008-01-02 11:53:07 +0000

    Created.

---
+++ src/dox.cpp
@@ -0,0 +1,48 @@
+/* ES40 emulator.
+ * Copyright (C) 2007-2008 by the ES40 Emulator Project
+ *
+ * Website: http://sourceforge.net/projects/es40
+ * E-mail : camiel@camicom.com
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Although this is not required, the author would appreciate being notified of, 
+ * and receiving any modifications you may make to the source code that might serve
+ * the general public.
+ */
+
+/**
+ * \file 
+ * Contains extra comments for Doxygen.
+ *
+ * You could read the documentation from this file; but it would probably
+ * be easier to go to http://es40.sourceforge.net.
+ *
+ * $Id: dox.cpp,v 1.1 2008/01/02 11:53:07 iamcamiel Exp $
+ *
+ * \author Camiel Vanderhoeven (camiel@camicom.com / http://www.camicom.com)
+ **/
+
+/**
+ * \mainpage ES40 Emulator Documentation
+ *
+ * \section intro Introduction
+ * Welcome to the documentation for the ES40 Emulator Project. For the
+ * main project page, goto http://sourceforge.net/projects/es40.
+ *
+ * On these pages here, you can find the documented source code. In the future,
+ * you might also find release notes, installation instructions, and compilation
+ * instructions here.
+ **/
