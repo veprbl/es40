65cacc673b67d227ac49f4ab43035730b28885f7
iamcamiel <iamcamiel>
2008-04-01 11:44:44 +0000

    *** empty log message ***

--- src/vms/AutoPtr.h
+++ src/vms/AutoPtr.h
@@ -1,7 +1,43 @@
+/* ES40 emulator.
+ * Copyright (C) 2007-2008 by the ES40 Emulator Project
+ *
+ * WWW    : http://sourceforge.net/projects/es40
+ * E-mail : camiel@camicom.com
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Although this is not required, the author would appreciate being notified of, 
+ * and receiving any modifications you may make to the source code that might serve
+ * the general public.
+ */
+
+/**
+ * \file 
+ * From the Poco C++ Libraries; used for ES40 for OpenVMS only, as Poco
+ * has dropped OpenVMS support.
+ *
+ * $Id: AutoPtr.h,v 1.2 2008/04/01 11:44:44 iamcamiel Exp $
+ *
+ * X-1.1        Camiel Vanderhoeven                             31-MAR-2008
+ *      Adapted for use by the ES40 Emulator.
+ **/
+
 //
 // AutoPtr.h
 //
-// $Id: AutoPtr.h,v 1.1 2008/03/31 19:13:28 iamcamiel Exp $
+// $Id: AutoPtr.h,v 1.2 2008/04/01 11:44:44 iamcamiel Exp $
 //
 // Library: Foundation
 // Package: Core
